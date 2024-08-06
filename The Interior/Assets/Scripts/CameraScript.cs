52cab4a139ba48e03c6e0ebc9752eb91dba6222d
Gabe-Antell-Proulx <gzap5573@gmail.com>
2024-08-06 10:35:10 -0400

    GOT CONTROLS WORKING!

--- The Interior/Assets/Scripts/CameraScript.cs
+++ The Interior/Assets/Scripts/CameraScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public float sensitivity = 1000f;
+    public float sensitivity = 100f;
     public Transform playerBody;
     float xRotation = 0f;
     float yRotation = 0f;
@@ -21,20 +21,15 @@ public class CameraScript : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
+        yRotation += mouseX;
+
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        // Rotate the camera (attached to the player's head) vertically
-        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        playerBody.rotation = Quaternion.Euler(0f, yRotation, 0f);
 
         // Rotate the player's body horizontally
-        playerBody.Rotate(Vector3.up * mouseX);
-        
-        //yRotation -= mouseX;
-        //yRotation = Mathf.Clamp(-90f, yRotation, 90f);
-
-        //transform.localRotation = Quaternion.Euler(0f, yRotation, 0f);
-
-        //playerBody.Rotate(Vector3.up * mouseY);
+        //playerBody.Rotate(Vector3.up * mouseX);
     }
 }
