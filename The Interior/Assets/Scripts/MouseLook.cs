22ff30494024cc712d9aaa80e82f72e54c13051b
Steven Luo <stevenluo2007@gmail.com>
2024-08-06 11:21:19 -0400

    Raytracing mouse works, textures not changed yet

--- The Interior/Assets/Scripts/MouseLook.cs
+++ The Interior/Assets/Scripts/MouseLook.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,7 +18,7 @@ public class MouseLook : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
@@ -31,6 +32,21 @@ public class MouseLook : MonoBehaviour
         // Rotate the player's body horizontally
         playerBody.Rotate(Vector3.up * mouseX);
 
-    
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, 1 << 7))
+        {
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            //print("There is something in front of the object!");
+
+            // OBJECT IS INTERACTABLE
+        }
+        else
+        {
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+
+            // OBJECT IS NOT INTERACTABLE
+        }
     }
 }
