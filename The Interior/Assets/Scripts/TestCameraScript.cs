6b1270014cc2d17e7e32ebff314c4a7066e5b6a6
Steven Luo <stevenluo2007@gmail.com>
2024-08-06 00:15:10 -0400

    Attempted to fix cam/player movement
    It didn't fully work, but... progress I guess?

---
+++ The Interior/Assets/Scripts/TestCameraScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraScript : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        playerBody.rotation = Quaternion.Euler(0f, yRotation, 0f);
+
+        // Rotate the player's body horizontally
+        //playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
