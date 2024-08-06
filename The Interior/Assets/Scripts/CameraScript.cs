e10a78029e717234b7ab44dbbc93707833f5fcbc
Gabe-Antell-Proulx <gzap5573@gmail.com>
2024-08-06 10:37:26 -0400

    Again?

--- The Interior/Assets/Scripts/CameraScript.cs
+++ The Interior/Assets/Scripts/CameraScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public float sensitivity = 100f;
+    public float sensitivity = 1500f;
     public Transform playerBody;
     float xRotation = 0f;
     float yRotation = 0f;
