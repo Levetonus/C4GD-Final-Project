38b52bb889478dd4ceb0dd668ba1446f4544e7da
Gabe-Antell-Proulx <gzap5573@gmail.com>
2024-08-08 10:34:47 -0400

    added to fileScript

--- The Interior/Assets/Scripts/FileScript.cs
+++ The Interior/Assets/Scripts/FileScript.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class FileScript : MonoBehaviour
 {
     public bool panelOpen = false;
+    public AudioSource source;
     public GameObject bKey;
     private void OnMouseDown()
     {
         bKey.SetActive(true);
+        source.Play();
     }
 }
